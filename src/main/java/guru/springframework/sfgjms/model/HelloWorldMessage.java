8ec66f922d4bfd6f31c4af0e2d87cc3859d895a0
Pardeep <pardeeps460@gmail.com>
2021-01-12 21:58:53 +0100

    introduced properties specifying the artemis username/password

---
+++ src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+    static final long serialVersionUID= 4812844162599589396L;
+    private UUID id;
+    private String message;
+}
